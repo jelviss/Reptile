6176e66e58bbbcfba0eee08e0872df7871a8f518
Elvis <hyqhyq2012123@163.com>
2015-05-11 20:26:25 +0800

    Optimized code style

--- trainTicketsSprider.py
+++ trainTicketsSprider.py
@@ -1,10 +1,7 @@
-#-*- coding:utf-8 -*-
-'''
+# -*- coding:utf-8 -*-
+# 功能:用于自动查询火车票余票
+# @author HuanYq
 
-功能:用于自动查询火车票余票
-@author HuanYq
-
-'''
 import requests
 import json
 import smtplib
@@ -16,48 +13,57 @@ class trainTicketsSprider:
     '''
     获得火车票的信息
     '''
-    def getTicketsInfo(self,purpose_codes,queryDate,from_station,to_station):
-        url = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=%s&queryDate=%s&from_station=%s&to_station=%s' %(purpose_codes,queryDate,from_station,to_station)
-        headers = { 
-                    "Accept":"text/html,application/xhtml+xml,application/xml;",
-                    "Accept-Encoding":"gzip",
-                    "Accept-Language":"zh-CN,zh;q=0.8",
-                    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
+    def getTicketsInfo(self, purpose_codes, queryDate,
+                       from_station, to_station):
+        url = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=%s&queryDate=%s&from_station=%s&to_station=%s' %(
+               purpose_codes, queryDate, from_station, to_station)
+        headers = {
+                    "Accept": "text/html,   \
+                    application/xhtml+xml,  \
+                    application/xml;",
+                    "Accept-Encoding": "gzip",
+                    "Accept-Language": "zh-CN,zh;q=0.8",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) \
+                    AppleWebKit/537.36 (KHTML, like Gecko) \
+                    Chrome/42.0.2311.90 Safari/537.36"
                   }
         TicketSession = requests.Session()
-        TicketSession.verify = False #关闭https验证   
+        TicketSession.verify = False  # 关闭https验证
         TicketSession.headers = headers
         try:
             resp_json = TicketSession.get(url)
             ticketsDatas = json.loads(resp_json.text)["data"]["datas"]
             return ticketsDatas
-        except Exception,e:
+        except Exception, e:
             print e
-            
-            
-class notifyMe:
+
+
+class notify:
     '''
     通过邮件将火车票的信息反馈给自己
     '''
-    def sendEmail(self,smtpserver,sender,receiver,username,password,subject,content):
-        msg = MIMEText(content,'html','utf8') 
+    def sendEmail(self, smtpserver, sender, receiver,
+                  username, password, subject, content):
+        msg = MIMEText(content, 'html', 'utf8')
         msg.set_charset('utf8')
         msg['Subject'] = subject
         msg['From'] = sender
         msg['To'] = receiver
         smtp = smtplib.SMTP()
         smtp.connect(smtpserver)
-        #smtp.set_debuglevel(1)
+        # smtp.set_debuglevel(1)
         smtp.login(username, password)
         smtp.sendmail(sender, receiver, msg.as_string())
         smtp.quit()
-    
+
+
 def isZero(num):
     if num == '--' or '无':
         return '0'
     else:
         return num
-    
+
+
 def main():
     '''
     ***************************
@@ -79,6 +85,7 @@ def main():
     username = 'example@163.com'
     password = 'yourpassword'
     relay = 120
+
     '''
     ***************************
     查询信息配置
@@ -92,18 +99,19 @@ def main():
     queryDate = '2015-05-23'
     from_station = 'NCG'
     to_station = 'CZQ'
+
     '''
     ***************************
     配置结束
-    ***************************
+    ****************************
     '''
-    
     TicketSprider = trainTicketsSprider()
-    res = TicketSprider.getTicketsInfo(purpose_codes,queryDate,from_station,to_station)
+    res = TicketSprider.getTicketsInfo(purpose_codes, queryDate,
+                                       from_station, to_station)
     contentlist = []
     content = ''
-    for i,ticketInfo in enumerate(res):     
-        contentlist.append("**********************************</br>")  
+    for i, ticketInfo in enumerate(res):
+        contentlist.append("**********************************</br>")
         contentlist.append(u"车次:%s</br>" %ticketInfo["station_train_code"])
         contentlist.append(u"起始站:%s</br>" %ticketInfo["start_station_name"])
         contentlist.append(u"目的地:%s</br>" %ticketInfo["to_station_name"])
@@ -115,7 +123,7 @@ def main():
         contentlist.append(u"无座还剩:%s张票</br>" %isZero(ticketInfo["wz_num"]))
         contentlist.append(u"是否有票:%s</br>" %ticketInfo["canWebBuy"])
         '''
-        print "**********************************"    
+        print "**********************************"
         print u"车次:%s" %ticketInfo["station_train_code"]
         print u"起始站:%s" %ticketInfo["start_station_name"]
         print u"目的地:%s" %ticketInfo["to_station_name"]
@@ -128,13 +136,12 @@ def main():
         print u"是否有票:%s" %ticketInfo["canWebBuy"]
         '''
     content = content.join(contentlist)
-    notify = notifyMe()
-    notify.sendEmail(smtpserver,sender,receiver,username,password,subject,content)
+    notifyme = notify()
+    notifyme.sendEmail(smtpserver, sender, receiver,
+                     username, password, subject, content)
     time.sleep(relay)
-    
-                
-                
-                
+
+
 if __name__ == '__main__':
     while(True):
         main()
