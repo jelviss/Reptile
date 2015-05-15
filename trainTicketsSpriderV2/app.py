d2b10130a4c07eebc1c122c4ee3b6454bcd90771
elvissg <hyqhyq2012123@163.com>
2015-05-15 00:02:58 +0000

    完成了web显示采页面显示采集队列

--- trainTicketsSpriderV2/app.py
+++ trainTicketsSpriderV2/app.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
 from flask import request
+from flask import redirect
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
 
@@ -10,6 +11,7 @@ from rq import Queue
 from redis import Redis
 from trainTicketsSprider import getandsend
 import time
+import cPickle
 
 import sys
 reload(sys)
@@ -21,13 +23,23 @@ RQDashboard(app)
 
 @app.route("/")
 def index():
-    args = None
+    r = Redis(host="127.0.0.1", port=6379, db=0)
+    res = r.lrange('email_que', 0 , 10)
+    args = []
+    for y in res:
+        args.append(eval(y))   
     return render_template('index.html', args=args)
 
 
-@app.route("/task", methods=['GET', 'POST'])
+@app.route("/task", methods=['POST'])
 def task():
     if request.method == 'POST':
+
+        sender = 'example@163.com'
+        subject = u'火车票信息'
+        smtpserver = 'smtp.163.com'
+        username = 'examole@163.com'
+        password = 'yourpassword'
 
         purpose_codes = request.form.get('ticketstype')
         queryDate = request.form.get('date')
@@ -35,25 +47,19 @@ def task():
         to_station = request.form.get('tostation')
         receiver = request.form.get('email')
 
-        sender = 'example@163.com'
-        subject = u'火车票信息'
-        smtpserver = 'smtp.163.com'
-        username = 'example@163.com'
-        password = 'yourpassword'
-
+        r = Redis(host="127.0.0.1", port=6379, db=0)
+        tickets_info = {'receiver':receiver, 'to_station':to_station, 'from_station':from_station, 'queryDate':queryDate, 'purpose_codes':purpose_codes}
+        r.lpush('email_que', str(tickets_info))
+        r.save()
+        
         q = Queue(connection=Redis())
         result = q.enqueue(
           getandsend, purpose_codes, queryDate, from_station, to_station, smtpserver, sender, receiver, username, password, subject
         )
 
-        return '票种:' + purpose_codes +'日期:' + queryDate + '出发站:' + from_station + '目的站:' + to_station + '接受者:' + receiver
-    
-    if request.method == 'GET':
-        if request.args:
-            return 'the arg is ' + request.args.get('email')
-        else:
-            return 'receive:nothing'
-     
+        #return '票种:' + purpose_codes +'日期:' + queryDate + '出发站:' + from_station + '目的站:' + to_station + '接受者:' + receiver
+        return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
