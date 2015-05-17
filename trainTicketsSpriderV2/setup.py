cd96bcced1729f86416e29c699a681ce3eee4600
Elvis <hyqhyq2012123@163.com>
2015-05-17 14:02:42 +0800

    ADD setup.py

---
+++ trainTicketsSpriderV2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os, subprocess, sys
+subprocess.call(['python', 'virtualenv.py', 'flask'])
+if sys.platform == 'win32':
+    bin = 'Scripts'
+else:
+    bin = 'bin'
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask<0.10'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'requests'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-bootstrap'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'supervisor'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'rq'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'rq-dashboard'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'redis'])
